37c77e74709d9c231af2463d25148280e1e0f4de
AgatheBecquart <inviteagathe@gmail.com>
2023-04-17 13:39:16 +0200

    initiation du projet

---
+++ top_films_series/top_films_series/spiders/top_films.py
@@ -0,0 +1,22 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+class CrawlerBooksSpider(CrawlSpider):
+    name = 'crawler_books'
+    allowed_domains = ['imdb.com']
+
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
+
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.imdb.com/chart/top/?ref_=nv_mv_250', headers={
+            'User-Agent': self.user_agent
+        })
+        
+class TopFilmsSpider(scrapy.Spider):
+    name = "top_films"
+    allowed_domains = ["www.imdb.com"]
+    start_urls = ["http://www.imdb.com/"]
+
+    def parse(self, response):
+        pass
