383ac7e5d68cdcc0359a3ca8f380aa415a7127a9
AgatheBecquart <inviteagathe@gmail.com>
2023-04-17 19:38:58 +0200

    création csv top_series

--- top_films_series/top_films_series/spiders/top_series.py
+++ top_films_series/top_films_series/spiders/top_series.py
@@ -22,14 +22,16 @@ class TopSeriesSpider(CrawlSpider):
     def parse_item(self, response):
         item = {}
         item['title'] = response.css('span.sc-afe43def-1.fDTGTb::text').extract()
-        # item['original_title'] = response.xpath('//div[contains(text(), "Original Title")]/following-sibling::text()').get().strip()
-        item['score'] = response.css('span.sc-bde20123-1.iZlgcd::text').extract()
+        item['original_title'] = response.css('div.sc-afe43def-3::text').extract()
+        item['score'] = response.css('span.sc-bde20123-1.iZlgcd::text')[0].extract()
         item['genre'] = response.css('a span.ipc-chip__text::text').extract()
         item['year'] = response.css('.ipc-inline-list__item a[href*="releaseinfo"]::text')[0].extract()
         # item['duration'] = response.xpath('//div[contains(text(), "Runtime")]/following-sibling::text()').get().strip()
-        # item['description'] = response.xpath('//div[contains(@class, "SummaryText")]//text()').get()
-        # item['cast'] = response.xpath('//h4[contains(text(), "Stars")]/following-sibling::a/text()').getall()
-        # item['certificate'] = response.xpath('//div[contains(text(), "Certificate")]/following-sibling::text()').get().strip()
-        # item['country'] = response.xpath('//div[contains(text(), "Country")]/following-sibling::a/text()').getall()
-        # item['language'] = response.xpath('//div[contains(text(), "Language")]/following-sibling::a/text()').getall()
+        item['description'] = response.css('span.sc-5f699a2-2::text').extract()
+        item['cast'] = response.css('.sc-52d569c6-3 a.ipc-metadata-list-item__list-content-item::text').extract()
+        item['public'] = response.css('.sc-afe43def-4 li:nth-of-type(3) a::text').extract()
+        item['country'] = response.css("[data-testid='title-details-origin'] a::text").extract()
+        item['language'] = response.css("[data-testid='title-details-languages'] a::text").extract()
+        #item['num_seasons'] = response.css("[data-testid='hero-title-block__metadata'] span[data-testid='TitleEpisodeCount']::text").extract_first()
+        #item['num_episodes'] = response.css("[data-testid='season-episodes'] .ipc-button__text::text").extract()
         yield item
