e9ca885c60b92c68d2c871ea06053e26e7c7bb17
Manuwest21 <hammers178@hotmail.com>
2023-04-20 12:50:30 +0200

    lien_video

--- top_films_series/top_films_series/spiders/top_films.py
+++ top_films_series/top_films_series/spiders/top_films.py
@@ -1,21 +1,7 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
-
-
-# from fake_useragent import UserAgent
-def convert_to_minutes(duration):
-    duration_str = ''.join(duration)  # Combine toutes les chaînes de la liste en une seule
-    parts = duration_str.split()  # Sépare les parties de la durée par des espaces
-    hours = 0
-    minutes = 0
-    for part in parts:
-        if part.endswith('h'):  # Si la partie se termine par 'h', c'est le nombre d'heures
-            hours = int(part[:-1])
-        elif part.endswith('m'):  # Si la partie se termine par 'm', c'est le nombre de minutes
-            minutes = int(part[:-1])
-    return hours*60 + minutes
-
+from fonctions import convert_to_minutes
 
 
 
